51098e25517e46da5b8e07d562bcc0e569c26d81
Aleksei <alekseylisovich@gmail.com>
2016-08-19 17:28:19 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStore_EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service dbService = new Service();
+            var phones = dbService.GetProductsWithMinPrice();
+
+            Console.WriteLine(phones.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStore_EF
+{
+    class Service
+    {
+        private dbEntities db = new dbEntities();
+
+        public ICollection<Phone> GetPhones()
+        {
+            return db.Phones.ToList();
+        }
+
+        public Phone GetProductsWithMinPrice()
+        {
+            return db.Phones
+                .OrderByDescending(p => p.Price)
+                .First();
+        }
+    }
+}
